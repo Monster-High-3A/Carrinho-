3410dfa568a6c573f1a26cfc554b02028139ab1e
pinezi0205 <78920618+pinezi0205@users.noreply.github.com>
2022-11-01 12:30:23 -0300

    Create codigo.ino

---
+++ Codigo carrinho/codigo.ino
@@ -0,0 +1,77 @@
+#define VA 2
+#define M1A 3
+#define M2A 4
+void setup() {
+  pinMode(VA, OUTPUT);
+  pinMode(M1A, OUTPUT);
+  pinMode(M2A, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+ if(Serial.available()>0){
+  char valorRecebido = Serial.read();
+  switch(valorRecebido){
+    case:
+     digitalWrite(M1A, HIGH);
+     digitalWrite(M2A, LOW);
+     digitalWrite(VA, 255);
+     digitalWrite(M1A, LOW);
+     digitalWrite(M2A, HIGH);
+     digitalWrite(VA, 255);
+    break; 
+
+    case 'F':
+      frente();
+    break; 
+
+    case 'B':
+      traz();
+    break; 
+
+    case 'R':
+      viraDireita();
+    break; 
+
+    case 'L':
+      viraEsquerda();
+    break; 
+
+    case 'S':
+      para();
+    break; 
+    
+  }
+ }
+
+}
+
+void frente(){
+     digitalWrite(M1A, HIGH);
+     digitalWrite(M2A, LOW);
+     digitalWrite(VA, 255);
+}
+void traz(){
+     digitalWrite(M1A, LOW);
+     digitalWrite(M2A, HIGH);
+     digitalWrite(VA, 255);
+}
+
+void viraDireita(){
+     digitalWrite(M1A, HIGH);
+     digitalWrite(M2A, LOW);
+     digitalWrite(VA, 255);
+}
+
+void viraEsquerda(){
+     digitalWrite(M1A, LOW);
+     digitalWrite(M2A, HIGH);
+     digitalWrite(VA, 255);
+}
+
+void para(){
+     digitalWrite(M1A, HIGH);
+     digitalWrite(M2A, LOW);
+     digitalWrite(VA, 255);
+}
+
