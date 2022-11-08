78509dc02b3a2d84e659f10b10f1e9bf207ea162
pinezi0205 <78920618+pinezi0205@users.noreply.github.com>
2022-11-08 12:21:05 -0300

    Update codigo.ino

--- Codigo carrinho/codigo.ino
+++ Codigo carrinho/codigo.ino
@@ -1,10 +1,17 @@
 #define VA 2
 #define M1A 3
 #define M2A 4
+#define VB 2
+#define M1B 3
+#define M2B 4
 void setup() {
   pinMode(VA, OUTPUT);
   pinMode(M1A, OUTPUT);
   pinMode(M2A, OUTPUT);
+  Serial.begin(9600);
+  pinMode(VB, OUTPUT);
+  pinMode(M1B, OUTPUT);
+  pinMode(M2B, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -12,15 +19,6 @@ void loop() {
  if(Serial.available()>0){
   char valorRecebido = Serial.read();
   switch(valorRecebido){
-    case:
-     digitalWrite(M1A, HIGH);
-     digitalWrite(M2A, LOW);
-     digitalWrite(VA, 255);
-     digitalWrite(M1A, LOW);
-     digitalWrite(M2A, HIGH);
-     digitalWrite(VA, 255);
-    break; 
-
     case 'F':
       frente();
     break; 
@@ -50,28 +48,43 @@ void frente(){
      digitalWrite(M1A, HIGH);
      digitalWrite(M2A, LOW);
      digitalWrite(VA, 255);
+     digitalWrite(M1B, LOW);
+     digitalWrite(M2B, HIGH);
+     digitalWrite(VB, 255);
 }
 void traz(){
      digitalWrite(M1A, LOW);
      digitalWrite(M2A, HIGH);
      digitalWrite(VA, 255);
+     digitalWrite(M1B, LOW);
+     digitalWrite(M2B, HIGH);
+     digitalWrite(VB, 255);
 }
 
 void viraDireita(){
      digitalWrite(M1A, HIGH);
      digitalWrite(M2A, LOW);
      digitalWrite(VA, 255);
+     digitalWrite(M1B, LOW);
+     digitalWrite(M2B, HIGH);
+     digitalWrite(VB, 255);
 }
 
 void viraEsquerda(){
      digitalWrite(M1A, LOW);
      digitalWrite(M2A, HIGH);
      digitalWrite(VA, 255);
+     digitalWrite(M1B, LOW);
+     digitalWrite(M2B, HIGH);
+     digitalWrite(VB, 255);
 }
 
 void para(){
      digitalWrite(M1A, HIGH);
      digitalWrite(M2A, LOW);
      digitalWrite(VA, 255);
+     digitalWrite(M1B, LOW);
+     digitalWrite(M2B, HIGH);
+     digitalWrite(VB, 255);
 }
 
